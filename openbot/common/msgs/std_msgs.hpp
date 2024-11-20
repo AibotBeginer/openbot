0a11d12ba3666f4d405c8848676eafb53b6d66cc
duyongquan <quandy2020@126.com>
2024-11-20 13:53:35 +0800

    [Fix] Fix map api
    	1 add map api
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- openbot/common/msgs/std_msgs.hpp
+++ openbot/common/msgs/std_msgs.hpp
@@ -34,6 +34,52 @@ struct Header
     std::string frame_id;
 };
 
+struct MultiArrayDimension
+{
+    std::string label;   // label of given dimension
+    uint32 size;         // size of given dimension (in type units)
+    uint32 stride;       // stride of given dimension
+};
+
+struct MultiArrayLayout
+{
+    // The multiarray declares a generic multi-dimensional array of a
+    // particular data type.  Dimensions are ordered from outer most
+    // to inner most.
+    //
+    // Accessors should ALWAYS be written in terms of dimension stride
+    // and specified outer-most dimension first.
+    //
+    // multiarray(i,j,k) = data[data_offset + dim_stride[1]*i + dim_stride[2]*j + k]
+    //
+    // A standard, 3-channel 640x480 image with interleaved color channels
+    // would be specified as:
+    //
+    // dim[0].label  = "height"
+    // dim[0].size   = 480
+    // dim[0].stride = 3*640*480 = 921600  (note dim[0] stride is just size of image)
+    // dim[1].label  = "width"
+    // dim[1].size   = 640
+    // dim[1].stride = 3*640 = 1920
+    // dim[2].label  = "channel"
+    // dim[2].size   = 3
+    // dim[2].stride = 3
+    //
+    // multiarray(i,j,k) refers to the ith row, jth column, and kth channel.
+
+    std::vector<MultiArrayDimension> dim;       // Array of dimension properties
+    uint32 data_offset;                         // padding bytes at front of data
+};
+
+struct Float32MultiArray
+{
+    // Please look at the MultiArrayLayout message definition for
+    // documentation on all multiarrays.
+
+    MultiArrayLayout layout;        // specification of data layout
+    std::vector<float> data;        // array of data
+};
+
 // Converts 'Header' to a openbot::common::proto::std_msgs::Header.
 openbot::common::proto::std_msgs::Header ToProto(const Header& data);
 
--- openbot/map/costmap.hpp
+++ openbot/map/costmap.hpp
@@ -18,6 +18,20 @@
 #define OPENBOT_MAP_COSTMAP_HPP_
 
 #include "openbot/common/macros.hpp"
+#include "openbot/common/msgs/msgs.hpp"
+#include "openbot/common/utils/ply.hpp"
+
+#include <string>
+#include <vector>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
 
 namespace openbot {
 namespace map { 
@@ -31,9 +45,113 @@ public:
     OPENBOT_SMART_PTR_DEFINITIONS(Costmap)
 
     /**
+     * @brief Default contructor
+     */
+    Costmap() = default;
+
+    /**
      * @brief Virtual destructor
      */
     virtual ~Costmap() {}
+
+    /**
+     * @brief Load octomap_filename convert to `pcl::PointCloud<pcl::PointXYZ>` format cloud
+     * 
+     * @param octomap_filename The octomap format filename
+     * @param cloud Ourput map data 
+     * @return return Sccess or failed
+     */
+    bool LoadOctomapFile(const std::string& octomap_filename, pcl::PointCloud<pcl::PointXYZ>& cloud);
+
+     /**
+     * @brief Load pcd_filename convert to `pcl::PointCloud<pcl::PointXYZ>` format cloud
+     * 
+     * @param pcd_filename The pcd format filename
+     * @param cloud Ourput map data 
+     * @return return Sccess or failed
+     */
+    bool LoadPCDFile(const std::string& pcd_filename, pcl::PointCloud<pcl::PointXYZ>& cloud);
+
+     /**
+     * @brief Load ply_filename convert to `pcl::PointCloud<pcl::PointXYZ>` format cloud
+     * 
+     * @param ply_filename The ply format filename
+     * @param cloud Ourput map data 
+     * @return return Sccess or failed
+     */
+    bool LoadPlyFile(const std::string& ply_filename, pcl::PointCloud<pcl::PointXYZ>& cloud);
+
+    /**
+     * @brief Map resolution
+     * @return Return map's resolution
+     */
+    double resolution();
+
+    /**
+     * @brief Map size x
+     * @return Return map's size x
+     */
+    double size_x();
+
+    /**
+     * @brief Map size y
+     * @return Return map's size y
+     */
+    double size_y();
+
+    /**
+     * @brief Map size z
+     * @return Return map's size z
+     */
+    double size_z();
+
+    /**
+     * @brief Map data size(x, y, z)
+     */
+    common::geometry_msgs::Point size(void) const;
+
+    /**
+     * @brief Map orgin position
+     */
+    common::geometry_msgs::Point origin() const;
+
+    /**
+     * @brief Map postion cost value
+     * @return  Return map's position cost value
+     */
+    double Cost(const double x, const double y, const double z);
+
+    /**
+     * @brief Check map data position(x, y, z) is freespace
+     * @return return Sccess or failed
+     */
+    bool IsFreespace(const double x, const double y, const double z);
+
+    /**
+     * @brief Check map data common::geometry_msgs::Point position(x, y, z) is freespace
+     * @return return Sccess or failed
+     */
+    template<typename T>
+    bool IsFreespace(const T& pose);
+
+    /**
+     * @brief Check map pose in Boundary
+     * @return return Sccess or failed
+     */
+    template<typename T>
+    bool CheckInBoundary(const T& pose);
+
+    /**
+     * @brief Convert pcl::PointCloud to occupancy grid map (2D)
+     * @return return Sccess or failed
+     */
+    bool ToOccupancyGridMap(common::nav_msgs::OccupancyGrid& data);
+
+    /**
+     * @brief Convert pcl::PointCloud to occupancy grid map (2D)
+     * @return return Sccess or failed
+     */
+    bool ToGridMap(common::nav_msgs::GridMap& data);
 
 };
 
