038f4c0e41d613058866ffee03dae070d17fca9f
duyongquan <quandy2020@126.com>
2024-12-18 14:12:37 +0800

    [Fix] Fix drivers module
    	1 Fix drivers module
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- openbot/drivers/components/camera_component.cc
+++ openbot/drivers/components/camera_component.cc
@@ -23,7 +23,17 @@ namespace components {
 
 bool CameraComponent::Init() 
 {
-  // raw_writer_ = node_->CreateWriter<Image>(camera_config_->raw_channel_name());
+  camera_config_ = std::make_shared<openbot::drivers::camera::config::Config>();
+  if (!apollo::cyber::common::GetProtoFromFile(config_file_path_, camera_config_.get())) 
+  {
+    return false;
+  }
+
+  AINFO << "Sensor camera output with: " << camera_config_->width();
+  AINFO << "Sensor camera output height: " << camera_config_->height();
+
+  writer_ = node_->CreateWriter<openbot_bridge::sensor_msgs::Image>(camera_config_->channel_name());
+  raw_writer_ = node_->CreateWriter<openbot_bridge::sensor_msgs::Image>(camera_config_->raw_channel_name());
   async_result_ = apollo::cyber::Async(&CameraComponent::run, this);
   return true;
 }
@@ -32,7 +42,20 @@ void CameraComponent::run()
 {
   running_.exchange(true);
   while (!apollo::cyber::IsShutdown()) {
-   
+
+    auto raw_image = std::make_shared<openbot_bridge::sensor_msgs::Image>();
+    raw_image->mutable_header()->set_frame_id(camera_config_->frame_id());
+    raw_image->set_width(camera_config_->width());
+    raw_image->set_height(camera_config_->height());
+    // raw_image->mutable_data()->reserve(raw_image_for_compress_->image_size);
+    raw_image->set_encoding(camera_config_->pixel_format());
+
+    for (int i = 0; i < camera_config_->width(); ++i) {
+      for (int j = 0; j < camera_config_->height(); ++i) {
+        
+      }
+    }
+
     // raw_writer_->Write(raw_image_for_compress);
     LOG(INFO) << "Publish images";
     apollo::cyber::SleepFor(std::chrono::seconds(1));
