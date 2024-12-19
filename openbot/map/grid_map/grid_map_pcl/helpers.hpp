7fc821fd2212178d01bdfcd73e60b65f4c3bdbd2
duyongquan <duyongquan3@jd.com>
2024-12-19 10:50:08 +0800

    [Fix] grid_map_pcl todo
    	1 todo tag grid_map_pcl
    
    Signed-off-by: duyongquan <duyongquan3@jd.com>

--- openbot/map/grid_map/grid_map_pcl/helpers.hpp
+++ openbot/map/grid_map/grid_map_pcl/helpers.hpp
@@ -19,24 +19,32 @@ class GridMap;
 
 namespace grid_map_pcl {
 
+// TODO(duyongquan)
 // void setVerbosityLevelToDebugIfFlagSet(const ros::NodeHandle& nh);
 
+// TODO(duyongquan)
 // std::string getParameterPath(const ros::NodeHandle& nh);
-
+// TODO(duyongquan)
 // std::string getOutputBagPath(const ros::NodeHandle& nh);
 
+// TODO(duyongquan)
 // std::string getPcdFilePath(const ros::NodeHandle& nh);
 
+// TODO(duyongquan)
 // std::string getMapFrame(const ros::NodeHandle& nh);
 
+// TODO(duyongquan)
 // std::string getMapRosbagTopic(const ros::NodeHandle& nh);
 
+// TODO(duyongquan)
 // std::string getMapLayerName(const ros::NodeHandle& nh);
 
+// TODO(duyongquan)
 // void saveGridMap(const grid_map::GridMap& gridMap, const ros::NodeHandle& nh, const std::string& mapTopic);
 
 inline void printTimeElapsedToRosInfoStream(const std::chrono::system_clock::time_point& start, const std::string& prefix);
 
+// TODO(duyongquan)
 // void processPointcloud(grid_map::GridMapPclLoader* gridMapPclLoader, const ros::NodeHandle& nh);
 
 using Point = ::pcl::PointXYZ;
