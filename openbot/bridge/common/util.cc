d20749da5da370dd777af5051a7ff92ef8e03ab4
duyongquan <quandy2020@126.com>
2024-12-26 14:51:19 +0800

    [Fix] add bridge sources
    	1 bridge
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ openbot/bridge/common/util.cc
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 The OpenRobotic Beginner Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "openbot/bridge/common/util.hpp"
+
+namespace openbot {
+namespace bridge {
+
+int GetProtoSize(const char *buf, size_t size) {
+  if (size != sizeof(size_t)) {
+    return 0;
+  }
+  char size_buf[sizeof(size_t)] = {0};
+  memcpy(size_buf, buf, sizeof(size_t));
+  int proto_size = *(reinterpret_cast<int *>(size_buf));
+  return proto_size;
+}
+
+}  // namespace bridge
+}  // namespace openbot
